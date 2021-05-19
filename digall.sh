bc4565abc637a98aebb0a463e057471638a1e3fd
nikesuputra <nike.suputra@gmail.com>
2021-05-19 13:27:01 +0800

    Fixed the text color

--- digall.sh
+++ digall.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-sucess="\033[0;93m"
-domain="\033[1;32m"
-msg="\033[0;37m"
+white="\033[0;37m";
+greenBold="\033[1;32m";
+yellowIntense="\033[0;93m";
+reset="\033[0m";
 
-echo "${msg}What is the domain name that you wish to dig?${domain}"
-read query
+echo -e "${white}What is the domain name that you wish to dig?${reset}";
+echo -e "${greenBold}";
+read query;
+echo -e "${reset}";
 
-A=$(dig $query A +noall +answer)
-AWWW=$(dig www.$query A +noall +answer)
-echo "\n${msg}A Record of ${domain}$query ${msg}and ${domain}www.$query ${sucess} \n$A \n$AWWW"
+A=$(dig A $query +noall +answer);
+AWWW=$(dig A www.$query +noall +answer);
+echo -e "${white}A Record of ${reset}${greenBold}$query${reset}${white} and ${reset}${greenBold}www.$query${reset}";
+echo -e "${yellowIntense}$A${reset}";
+echo -e "${yellowIntense}$AWWW${reset}";
+echo -e "\n";
 
-AAAA=$(dig $query AAAA +noall +answer)
-AAAAWWW=$(dig www.$query AAAA +noall +answer)
-echo "\n${msg}AAAA Record of ${domain}$query ${msg}and ${domain}www.$query ${sucess} \n$AAAA \n$AAAAWWW"
+AAAA=$(dig AAAA $query +noall +answer @8.8.4.4);
+AAAAWWW=$(dig AAAA www.$query +noall +answer @8.8.4.4);
+echo -e "${white}AAAA Record of ${reset}${greenBold}$query${reset}${white} and ${reset}${greenBold}www.$query${reset}";
+echo -e "${yellowIntense}$AAAA${reset}";
+echo -e "${yellowIntense}$AAAAWWW${reset}";
+echo -e "\n";
 
-CNAME=$(dig $query CNAME +noall +answer) 
-CNAMEWWW=$(dig www.$query CNAME +noall +answer) 
-echo "\n${msg}CNAME Record of ${domain}$query ${msg}and ${domain}www.$query ${sucess} \n$CNAME \n$CNAMEWWW"
+CNAME=$(dig CNAME $query +noall +answer @8.8.4.4);
+CNAMEWWW=$(dig CNAME www.$query +noall +answer @8.8.4.4);
+echo -e "${white}CNAME Record of ${reset}${greenBold}$query${reset}${white} and ${reset}${greenBold}www.$query${reset}";
+echo -e "${yellowIntense}$CNAME${reset}";
+echo -e "${yellowIntense}$CNAMEWWW${reset}";
+echo -e "\n";
 
-MX=$(dig $query MX +noall +answer)
-echo "\n${msg}MX Record of ${domain}$query ${sucess} \n$MX"
+MX=$(dig MX $query +noall +answer @8.8.4.4);
+echo -e "${white}MX Record of ${reset}${greenBold}$query${reset}";
+echo -e "${yellowIntense}$MX${reset}";
+echo -e "\n";
 
-NS=$(dig $query NS +noall +answer)
-echo "\n${msg}NS Record of ${domain}$query ${sucess} \n$NS"
+NS=$(dig NS $query +noall +answer @8.8.4.4);
+echo -e "${white}NS Record of ${reset}${greenBold}$query${reset}";
+echo -e "${yellowIntense}$NS${reset}";
+echo -e "\n";
 
-TXT=$(dig $query TXT +noall +answer)
-echo "\n${msg}TXT Record of ${domain}$query ${sucess} \n$TXT"
+TXT=$(dig TXT $query +noall +answer @8.8.4.4);
+echo -e "${white}TXT Record of ${reset}${greenBold}$query${reset}";
+echo -e "${yellowIntense}$TXT${reset}";
+echo -e "\n";
 
-CAA=$(dig $query CAA +noall +answer)
-echo "\n${msg}CAA Record of ${domain}$query ${sucess} \n$CAA"
+CAA=$(dig CAA $query +noall +answer @8.8.4.4);
+echo -e "${white}CAA Record of ${reset}${greenBold}$query${reset}";
+echo -e "${yellowIntense}$CAA${reset}";
